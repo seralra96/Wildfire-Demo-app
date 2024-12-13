abd4ed0e462431cf043e8f69644561005cd4844d
Sergio Ramírez <seralra96@gmail.com>
2024-12-13 14:33:42 -0500

    Use reverse proxy app in render.com instead local service

--- pages/1_PlanetScope_Basemap.py
+++ pages/1_PlanetScope_Basemap.py
@@ -5,13 +5,13 @@ import os
 import atexit
 
 # Start the proxy server as a background process
-proxy_process = subprocess.Popen(['python', 'proxy_server.py'])
+# proxy_process = subprocess.Popen(['python', 'proxy_server.py'])
 
 # Ensure the proxy server is terminated when the app is closed
-def stop_proxy():
-    proxy_process.terminate()
+# def stop_proxy():
+#     proxy_process.terminate()
 
-atexit.register(stop_proxy)
+# atexit.register(stop_proxy)
 
 st.set_page_config(layout="wide")
 
@@ -35,13 +35,15 @@ with st.expander("See source code"):
     with st.echo():
         m = leafmap.Map(center=[-14.2, -63.11], zoom=10)
         m.add_tile_layer(
-            url="http://localhost:5000/tiles/global_monthly_2024_08_mosaic/gmap/{z}/{x}/{y}.png",
+            #url="http://localhost:5000/tiles/global_monthly_2024_08_mosaic/gmap/{z}/{x}/{y}.png",
+            url="https://reverse-proxy-basemaps.onrender.com/tiles/global_monthly_2024_08_mosaic/gmap/{z}/{x}/{y}.png",
             name="Before event",
             #opacity=opacity_before,
             attribution="© Planet Labs"
         )
         m.add_tile_layer(
-            url="http://localhost:5000/tiles/global_monthly_2024_10_mosaic/gmap/{z}/{x}/{y}.png",
+            #url="http://localhost:5000/tiles/global_monthly_2024_10_mosaic/gmap/{z}/{x}/{y}.png",
+            url="https://reverse-proxy-basemaps.onrender.com/tiles/global_monthly_2024_10_mosaic/gmap/{z}/{x}/{y}.png",
             name="After event",
             opacity=opacity_after,
             attribution="© Planet Labs"
